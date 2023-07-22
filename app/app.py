f81673a319a5d77b6a943b7b200159d154607f97
Sourav Suvarna <96716586+souravsuvarna@users.noreply.github.com>
2023-07-22 23:41:57 +0530

    Update app.py

--- app/app.py
+++ app/app.py
@@ -2,7 +2,7 @@ import streamlit as st
 import pandas as pd
 import preprocess, basic_impute, advance_impute
 
-st.set_page_config(page_title="MissNoMore", page_icon="🔍")
+st.set_page_config(page_title="MissNoMore", page_icon="🌐")
 css = '''
 <style>
     [data-testid="stSidebar"]{
